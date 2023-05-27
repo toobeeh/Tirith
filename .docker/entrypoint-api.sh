a12b01271c355ac4eb8be2425f5193153de7308d
tobeh <tobiasch@gmx.at>
2023-05-27 12:46:06 +0200

    add shebang

--- .docker/entrypoint-api.sh
+++ .docker/entrypoint-api.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 echo $DB_DOMAIN_NAME
 sed -i "s/DB_DOMAIN_NAME_SED_PLACEHOLDER/$DB_DOMAIN_NAME/g" /app/tirith-api/dist/config/production.config.js
 cat /app/tirith-api/dist/config/production.config.js
--- .docker/entrypoint-frontend.sh
+++ .docker/entrypoint-frontend.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 sed -i "s/API_DOMAIN_NAME_SED_PLACEHOLDER/$API_DOMAIN_NAME/g" /usr/share/nginx/html/main.*.js
 sh -c nginx -g 'daemon off;'
