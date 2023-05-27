29a2b193534bc1aafd653ed6cc83f36a3f772692
tobeh <tobiasch@gmx.at>
2023-05-27 13:18:53 +0200

    add cmd to dockerfile

--- .docker/entrypoint-api.sh
+++ .docker/entrypoint-api.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-sed -i "s/DB_DOMAIN_NAME_SED_PLACEHOLDER/$DB_DOMAIN_NAME/g" /app/tirith-api/dist/config/production.config.js
-sh -c node /app/tirith-api/dist/main.js
+set -e
+sed -i "s/DB_DOMAIN_NAME_SED_PLACEHOLDER/$DB_DOMAIN_NAME/g" /app/tirith-api/dist/config/production.config.js 
+
+# Start the main process
+exec "$@"
