89f47fa0ca44ffccfaa44c3a51207a34b5e9aa56
tobeh <tobiasch@gmx.at>
2023-05-27 02:56:16 +0200

    fix fucking file ending typo

--- .docker/entrypoint-api.sh
+++ .docker/entrypoint-api.sh
@@ -1,2 +1,2 @@
-sed -i "s/DB_DOMAIN_NAME_SED_PLACEHOLDER/$DB_DOMAIN_NAME/g" /app/tirith-api/dist/config/production.config.js.ts
+sed -i "s/DB_DOMAIN_NAME_SED_PLACEHOLDER/$DB_DOMAIN_NAME/g" /app/tirith-api/dist/config/production.config.js
 sh -c node /app/tirith-api/dist/main.js
