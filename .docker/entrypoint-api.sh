a1afa1d12a6981055ae206ae38e58db3103624bd
tobeh <tobiasch@gmx.at>
2023-05-27 12:53:46 +0200

    finally fix env and reduce img size

--- .docker/entrypoint-api.sh
+++ .docker/entrypoint-api.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-echo $DB_DOMAIN_NAME
 sed -i "s/DB_DOMAIN_NAME_SED_PLACEHOLDER/$DB_DOMAIN_NAME/g" /app/tirith-api/dist/config/production.config.js
-cat /app/tirith-api/dist/config/production.config.js
 sh -c node /app/tirith-api/dist/main.js
