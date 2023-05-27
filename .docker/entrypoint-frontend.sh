ba31fe9d3c64c8e73ef7fe4c655b6bcdc25308fc
tobeh <tobiasch@gmx.at>
2023-05-27 13:31:26 +0200

    entrypoint for frontend

--- .docker/entrypoint-frontend.sh
+++ .docker/entrypoint-frontend.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+set -e
 sed -i "s/API_DOMAIN_NAME_SED_PLACEHOLDER/$API_DOMAIN_NAME/g" /usr/share/nginx/html/main.*.js
-sh -c nginx -g 'daemon off;'
+
+# Start the main process
+exec "$@"
