4e09e3669a119ea6e0553745c5ce149c39f91cb4
tobeh <tobiasch@gmx.at>
2023-12-19 23:31:56 +0100

    add dev config again

---
+++ tirith-api/src/config/development.config.ts
@@ -0,0 +1,10 @@
+export default () => ({
+    PORT: 3000,
+    DB_HOST: "mariadb.typo.rip",
+    DB_USER: "ithil",
+    DB_PASSWORD: "",
+    DISCORD_OAUTH_CLIENT_ID: 'xyz',
+    DISCORD_OAUTH_CLIENT_SECRET: 'xyz',
+    DISCORD_OAUTH_REDIRECT: 'http://localhost:4200/auth',
+    DISCORD_API_TOKEN: 'xyz'
+});
