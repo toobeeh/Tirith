cafb0868e6ea54d0777ab2021222202f42f9f7bd
tobeh <tobiasch@gmx.at>
2024-02-15 17:56:45 +0100

    update dev base config

--- tirith-api/src/config/development.config.ts
+++ tirith-api/src/config/development.config.ts
@@ -1,10 +1,8 @@
 export default () => ({
     PORT: 3000,
-    DB_HOST: "mariadb.typo.rip",
-    DB_USER: "ithil",
-    DB_PASSWORD: "",
-    DISCORD_OAUTH_CLIENT_ID: 'xyz',
-    DISCORD_OAUTH_CLIENT_SECRET: 'xyz',
-    DISCORD_OAUTH_REDIRECT: 'http://localhost:4200/auth',
-    DISCORD_API_TOKEN: 'xyz'
+    DISCORD_OAUTH_CLIENT_ID: '',
+    DISCORD_OAUTH_CLIENT_SECRET: '',
+    DISCORD_OAUTH_REDIRECT: '',
+    DISCORD_API_TOKEN: '',
+    GRPC_VALMAR_CHANNEL: ''
 });
