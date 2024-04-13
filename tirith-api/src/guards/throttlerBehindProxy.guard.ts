2b66426833550e610b3f454b24f7ba9c12613007
tobeh <tobiasch@gmx.at>
2024-04-14 00:08:31 +0200

    log req ips

--- tirith-api/src/guards/throttlerBehindProxy.guard.ts
+++ tirith-api/src/guards/throttlerBehindProxy.guard.ts
@@ -4,6 +4,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class ThrottlerBehindProxyGuard extends ThrottlerGuard {
     protected getTracker(req: Record<string, any>): Promise<string> {
+        console.log(req.ips);
         return new Promise<string>((resolve, reject) => {
             const tracker = req.ips.length > 0 ? req.ips[0] : req.ip; // individualize IP extraction to meet your own needs
             resolve(tracker);
