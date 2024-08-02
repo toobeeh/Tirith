2264b90970d4ee10eb0e70c619bb648ef1842d76
tobeh <tobiasch@gmx.at>
2024-08-02 18:40:10 +0200

    fix scene list event

--- tirith-api/src/modules/grpc/services/grpc-scenes.service.ts
+++ tirith-api/src/modules/grpc/services/grpc-scenes.service.ts
@@ -2,23 +2,30 @@
 https://docs.nestjs.com/providers#services
 */
 
-import { Injectable } from '@nestjs/common';
+import {Inject, Injectable} from '@nestjs/common';
 import { IScenesService } from '../../../services/interfaces/scenes.service.interface';
 import { SceneDto } from '../../palantir/dto/scenes.dto';
 import { ConfigService } from '@nestjs/config';
 import { ScenesDefinition } from '../proto-compiled/scenes';
 import { GrpcBaseService } from './grpc-base';
+import {GrpcEventsService} from "./grpc-events.service";
+import {IGuildsService} from "../../../services/interfaces/guilds.service.interface";
+import {IMembersService} from "../../../services/interfaces/members.service.interface";
+import {IEventsService} from "../../../services/interfaces/events.service.interface";
 
 @Injectable()
 export class GrpcScenesService extends GrpcBaseService<ScenesDefinition> implements IScenesService {
 
-    constructor(config: ConfigService) {
+    constructor(config: ConfigService, @Inject(IEventsService) private eventsService: GrpcEventsService) {
         super(ScenesDefinition, config);
     }
 
     async getAllScenes(): Promise<SceneDto[]> {
+        const events = await this.eventsService.getAllEvents();
+
         const iter = this.grpcClient.getAllScenes({});
-        const scenes = await this.collectFromAsyncIterable(iter);
+        let scenes = await this.collectFromAsyncIterable(iter);
+        scenes = scenes.map(scene => ({...scene, event: events.find(event => event.id == scene.eventId)}));
         return scenes;
     }
 
