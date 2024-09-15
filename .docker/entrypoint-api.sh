e753ca0883dc82429d42bc3ee14d9d037ce6d873
tobeh <tobiasch@gmx.at>
2024-09-16 00:58:49 +0200

    fix sed priority

--- .docker/entrypoint-api.sh
+++ .docker/entrypoint-api.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
-sed -i "s!GRPC_CHANNEL_SED_PLACEHOLDER!$GRPC_CHANNEL!g" /app/tirith-api/dist/config/production.config.js
 sed -i "s!CONTENT_GRPC_CHANNEL_SED_PLACEHOLDER!$CONTENT_GRPC_CHANNEL!g" /app/tirith-api/dist/config/production.config.js
+sed -i "s!GRPC_CHANNEL_SED_PLACEHOLDER!$GRPC_CHANNEL!g" /app/tirith-api/dist/config/production.config.js
 sed -i "s/DISCORD_OAUTH_CLIENT_ID_SED_PLACEHOLDER/$DISCORD_OAUTH_CLIENT_ID/g" /app/tirith-api/dist/config/production.config.js 
 sed -i "s/DISCORD_OAUTH_CLIENT_SECRET_SED_PLACEHOLDER/$DISCORD_OAUTH_CLIENT_SECRET/g" /app/tirith-api/dist/config/production.config.js 
 sed -i "s!DISCORD_OAUTH_REDIRECT_SED_PLACEHOLDER!$DISCORD_OAUTH_REDIRECT!g" /app/tirith-api/dist/config/production.config.js 
