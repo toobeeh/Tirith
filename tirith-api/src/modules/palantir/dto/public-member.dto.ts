31048f8223520cce2d1b00a076bdf26988766f6c
tobeh <66030584+toobeeh@users.noreply.github.com>
2025-07-20 11:48:29 +0200

    #4 add login as number to public member dto

--- tirith-api/src/modules/palantir/dto/public-member.dto.ts
+++ tirith-api/src/modules/palantir/dto/public-member.dto.ts
@@ -9,8 +9,11 @@ export class PublicMemberDto {
     @XApiProperty({ description: "The member's palantir accunt user name" })
     userName: string;
 
-    @XApiProperty({ description: "The member's palantir identification" })
+    @XApiProperty({ description: "DEPRECATED. Use TypoId instead.", deprecated: true })
     userLogin: string;
+
+    @XApiProperty({ description: "The member's typo unique identification" })
+    typoId: number;
 
     @XApiProperty({ description: "Enum array containing the flags of the member", enum: MemberFlagDto, isArray: true })
     memberFlags: MemberFlagDto[];
