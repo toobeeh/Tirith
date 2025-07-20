73a884b63a19b819e98db054fdf991b54f10b366
tobeh <66030584+toobeeh@users.noreply.github.com>
2025-07-20 12:43:07 +0200

    #5 improve scenes list layout and show themes

--- tirith-frontend/src/app/public/pages/tools/scenes/scenes.component.ts
+++ tirith-frontend/src/app/public/pages/tools/scenes/scenes.component.ts
@@ -1,8 +1,7 @@
-import { CommonModule } from "@angular/common";
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { FormControl } from "@angular/forms";
 import { debounceTime, map } from "rxjs";
-import { SceneDto, ScenesService } from "src/api";
+import {SceneDto, ScenesService, SceneThemeDto} from "src/api";
 
 @Component({
   selector: 'app-scenes',
@@ -17,6 +16,7 @@ import { SceneDto, ScenesService } from "src/api";
 export class ScenesComponent {
 
   private scenes: SceneDto[] = [];
+  private currentThemes: Map<number, SceneThemeDto | undefined> = new Map<number, SceneThemeDto | undefined>();
   public filterInput = new FormControl("");
 
   public scenes$;
@@ -24,7 +24,7 @@ export class ScenesComponent {
   constructor(scenesService: ScenesService) {
 
     this.scenes$ = this.filterInput.valueChanges
-      .pipe(debounceTime(200), map(() => this.filterSprites()));
+      .pipe(debounceTime(200), map(() => this.filterScenes()));
 
     scenesService.getAllScenes().subscribe(scenes => {
       this.scenes = scenes;
@@ -32,7 +32,7 @@ export class ScenesComponent {
     });
   }
 
-  private filterSprites() {
+  private filterScenes() {
     const filter = this.filterInput.value ?? "";
 
     return this.scenes
@@ -40,13 +40,31 @@ export class ScenesComponent {
       .filter(s => s.tags.some(tag => tag.toLocaleLowerCase().includes(filter.toLocaleLowerCase())));
   }
 
-  private getSceneTags(scenes: SceneDto) {
+  public getSceneTags(scene: SceneDto) {
     const tags = [];
-    tags.push(scenes.name);
-    if (scenes.artist) tags.push(scenes.artist);
-    if (scenes.event) tags.push(scenes.event.name);
-    if (!scenes.event) tags.push("regular");
+    tags.push(scene.name);
+    if (scene.artist) tags.push(scene.artist);
+    if (scene.event) tags.push(scene.event.name);
+    if (!scene.event) tags.push("regular");
+    if(scene.themes.length > 0) tags.push("themes");
+    const theme = this.currentThemes.get(scene.id);
+    if (theme) {
+      tags.push(`#${theme.name}`);
+    }
 
     return tags.map(t => `#${t}`);
+  }
+
+  public cycleSceneTheme(scene: SceneDto) {
+    if(scene.themes.length === 0) return;
+
+    const currentThemeIndex = scene.themes.findIndex(t => t.shift === this.currentThemes.get(scene.id)?.shift);
+    const nextThemeIndex = (currentThemeIndex + 1) % (scene.themes.length + 1);
+    this.currentThemes.set(scene.id, nextThemeIndex === scene.themes.length ? undefined : scene.themes[nextThemeIndex]);
+  }
+
+  public getSceneUrl(scene: SceneDto): string {
+    const theme = this.currentThemes.get(scene.id);
+    return theme ? `https://static.typo.rip/sprites/rainbow/modulate.php?url=${scene.url}&hue=${theme.shift}` : scene.url;
   }
 }
